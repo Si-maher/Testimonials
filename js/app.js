4b2f5bdbed2ab45201f8c3ec865b9ccb07c6548e
Simon Maher <simonmaher8307@hotmail.com>
2019-10-14 10:17:15 +0300

    created object constructor function

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// immediately invoked function 
+
+(function() {
+// customers 
+let customers = []
+let index = 0
+
+// object constructor function 
+
+function Customer(name, img, text){
+    this.name = name;
+    this.img = img;
+    this.text = text 
+}
+})
